7223002a2e8f058dbbb4d978f3ea8e445442bb8c
hojioM <84308563+hojioM@users.noreply.github.com>
2022-08-20 18:40:03 +0800

    Update iLovePDF.js

--- iLovePDF/iLovePDF.js
+++ iLovePDF/iLovePDF.js
@@ -1,4 +1,4 @@
 [rewrite_local]
-^https:\/\/service\.ilovepdf\.com\/v1\/user$ url response-body "premium":.*?false, response-body "premium":true,
+^https?:\/\/service\.ilovepdf\.com\/v1\/user$ url response-body "premium":*?false, response-body "premium":true,
 [mitm] 
 hostname = service.ilovepdf.com
