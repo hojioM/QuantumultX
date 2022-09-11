2d2bc2b6bc01c82de1a3b0b3827f42a0f3aaa21f
hojioM <84308563+hojioM@users.noreply.github.com>
2022-09-11 12:20:05 +0800

    Update VpnPrime.js

--- VpnPrime/VpnPrime.js
+++ VpnPrime/VpnPrime.js
@@ -1,9 +1,10 @@
+/*
 [mitm]
 hostname = *.cloudfront.net
 [rewrite_local]
 ^https:?\/\/.*\.cloudfront\.net\/api\/clients\/info url script-response-body https://raw.githubusercontent.com/hojioM/QuantumultX/main/VpnPrime/VpnPrime.js
 ^https:?\/\/.*\.cloudfront\.net\/api\/purchases\/validate url script-response-body https://raw.githubusercontent.com/hojioM/QuantumultX/main/VpnPrime/VpnPrime.js
-
+*/
 let obj = JSON.parse($response.body);
 obj["expiration_time"] = 4101790766;
 $done({body: JSON.stringify(obj)});
